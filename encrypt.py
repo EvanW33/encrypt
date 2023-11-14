068896e88c73542a596d80c428882733cfa5df10
Evan Wilson <42522621+EvanW33@users.noreply.github.com>
2023-11-14 07:37:32 -0700

    Add files via upload
    Removed cryptography library as one has not been selected yet, in research phase of determining a library.

--- encrypt.py
+++ encrypt.py
@@ -1,27 +1,22 @@
-from cryptography.fernet import Fernet
 import hashlib
+import os
 
 
 def get_file(filename):
   # This function gets the file name of what will be encrypted/decrypted
   pass
 
-def get_password(new_password=False):
-  # This function asks for the password and returns the SHA-256 key
-  if new_password:
-    # First time running, this is the encrypting phase
-    pass
-  else:
-    # This is decrypting phase
-    key = hashlib.sha256(input("Enter password:\n").encode('utf-8')).hexdigest()
-  return key
+def private_key_generation(new_password=False):
+  # This function asks for the password and private key generated using PBKDF2 and a salt
+  return hashlib.sha256(input("Enter password:\n").encode('utf-8')).hexdigest()
 
 
-def encrypt():
+def encrypt(key):
+  # Encrypts the file using the inputted key
   pass
 
 
-def decrypt():
+def decrypt(key):
   pass
 
 
@@ -32,15 +27,18 @@ def main():
     if(option.lower() == "encrypt" or option.lower() == "decrypt"):
       break
 
+  # Gets the key that will be used to encrypt/decrypt the file
+  key = private_key_generation()
+
   # Determines what to do next
   if(option.lower() == "encrypt"):
-    encrypt()
+    encrypt(key)
   elif(option.lower() == "decrypt"):
-    decrypt()
+    decrypt(key)
   else:
     raise Exception("Unauthorized text")
-  key = get_password()
-  print(key)
+
+  
     
 
 if __name__ == "__main__":
