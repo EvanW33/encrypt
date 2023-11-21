95269b691435f2e19684a9e8d8a36ea4235d344c
Evan Wilson <42522621+EvanW33@users.noreply.github.com>
2023-11-21 16:34:26 -0700

    Add files via upload
    Separated the prompt for input into its own function with prompt for getting the filename. Implemented Fernet.generate_key() to generate a key, write it to a file, and print it to stdout.

--- encrypt.py
+++ encrypt.py
@@ -1,18 +1,39 @@
 import hashlib
 import os
+from cryptography.fernet import Fernet
 
 
 def get_file(filename):
   # This function gets the file name of what will be encrypted/decrypted
   pass
 
-def private_key_generation(new_password=False):
-  # This function asks for the password and private key generated using PBKDF2 and a salt
-  return hashlib.sha256(input("Enter password:\n").encode('utf-8')).hexdigest()
+
+def key_generation():
+  # Creates the private key
+  key = Fernet.generate_key()
+
+  with open("crypto.key", "wb") as k:
+    k.write(key)
+  return key
+
+
+def print_prompts(prompt):
+  # Used to print prompts that are needed for input
+  if prompt == 1: # Gets input for what action (encrypt or decrypt) is going to be performed
+    while True:
+      # Gets input until encrypt, e, decrypt, or d is entered
+      option = input("What would you like to do; Encrypt(E) or Decrypt(D) a file?")
+      if(option.lower() == "encrypt" or option.lower() == "decrypt" or option.lower() == "d" or option.lower() == "e"):
+        return option
+    
+  if prompt == 2: # Gets input for the filename that will be encrypted or decrypted
+    filename = input("Please enter the file and file extension:\n")
+    return filename
+
 
 
 def encrypt(key):
-  # Encrypts the file using the inputted key
+  # Encrypts the file using the key
   pass
 
 
@@ -21,25 +42,21 @@ def decrypt(key):
 
 
 def main():
-  while True:
-    # Gets input until encrypt or decrypt is input
-    option = input("What would you like to do; encrypt or decrypt a file?\nEnter \"Encrypt\" or \"Decrypt\"\n")
-    if(option.lower() == "encrypt" or option.lower() == "decrypt"):
-      break
-
   # Gets the key that will be used to encrypt/decrypt the file
-  key = private_key_generation()
+  key = key_generation()
+  print("Key: {}".format(key))
+
+  # Gets what will be done
+  option = print_prompts(1)  
 
   # Determines what to do next
-  if(option.lower() == "encrypt"):
+  if(option.lower() == "encrypt" or option.lower() == "e"):
     encrypt(key)
-  elif(option.lower() == "decrypt"):
+  elif(option.lower() == "decrypt" or option.lower() == "d"):
     decrypt(key)
   else:
     raise Exception("Unauthorized text")
 
-  
-    
 
 if __name__ == "__main__":
   main()
