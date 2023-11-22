078e15791bf80384cbe5c9d5cd3ddfea244a0c68
Evan Wilson <42522621+EvanW33@users.noreply.github.com>
2023-11-21 17:02:38 -0700

    Add files via upload

--- encrypt.py
+++ encrypt.py
@@ -8,13 +8,19 @@ def get_file(filename):
   pass
 
 
+def get_key():
+  # Reads and returns the key from crypto.key
+  with open("crypto.key", "rb") as file:
+    key = file.read()
+    return key
+
+
 def key_generation():
   # Creates the private key
   key = Fernet.generate_key()
 
   with open("crypto.key", "wb") as k:
     k.write(key)
-  return key
 
 
 def print_prompts(prompt):
@@ -22,28 +28,37 @@ def print_prompts(prompt):
   if prompt == 1: # Gets input for what action (encrypt or decrypt) is going to be performed
     while True:
       # Gets input until encrypt, e, decrypt, or d is entered
-      option = input("What would you like to do; Encrypt(E) or Decrypt(D) a file?")
+      option = input("What would you like to do; Encrypt(E) or Decrypt(D)?\n")
       if(option.lower() == "encrypt" or option.lower() == "decrypt" or option.lower() == "d" or option.lower() == "e"):
         return option
     
   if prompt == 2: # Gets input for the filename that will be encrypted or decrypted
-    filename = input("Please enter the file and file extension:\n")
-    return filename
+    while True:
+      filename = input("Please enter the file and file extension:\n")
+      if os.path.exists(filename):
+        print("The file exists!")
+        return filename
+      else:
+        print("Invalid file name!")
 
 
 
 def encrypt(key):
   # Encrypts the file using the key
-  pass
+  f = Fernet(key)
 
 
 def decrypt(key):
+  # Decrypts the file using the key
   pass
 
 
 def main():
   # Gets the key that will be used to encrypt/decrypt the file
-  key = key_generation()
+  if not os.path.exists("crypto.key"):
+    print("Created new crypto.key file")
+    key_generation()
+  key = get_key()
   print("Key: {}".format(key))
 
   # Gets what will be done
@@ -56,6 +71,9 @@ def main():
     decrypt(key)
   else:
     raise Exception("Unauthorized text")
+  
+  # Gets the file to be encrypted/decrypted
+  filename = print_prompts(2)
 
 
 if __name__ == "__main__":
